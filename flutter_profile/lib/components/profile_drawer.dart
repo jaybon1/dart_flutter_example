0f21d26dbb1b64a7d4b3da37c5cbec7613f91c65
jaybon1 <ponggas2@gmail.com>
2023-03-07 09:19:44 +0900

    드로어 변경

--- flutter_profile/lib/components/profile_drawer.dart
+++ flutter_profile/lib/components/profile_drawer.dart
@@ -3,13 +3,17 @@ import 'package:flutter/material.dart';
 class ProfileDrawer extends StatelessWidget {
   const ProfileDrawer({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 200,
-      height: double.infinity,
-      color: Colors.blue,
+    return SafeArea(
+      child: Container(
+        width: 200,
+        height: double.infinity,
+        color: Colors.white,
+        child: Row(
+          children: [],
+        ),
+      ),
     );
   }
 }
