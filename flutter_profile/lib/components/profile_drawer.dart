c49eadf944f51853fcc6a6396ab97bb8a5264618
jaybon1 <ponggas2@gmail.com>
2023-03-07 09:55:23 +0900

    드로어 item 추가

--- flutter_profile/lib/components/profile_drawer.dart
+++ flutter_profile/lib/components/profile_drawer.dart
@@ -10,10 +10,30 @@ class ProfileDrawer extends StatelessWidget {
         width: 200,
         height: double.infinity,
         color: Colors.white,
-        child: Row(
-          children: [],
+        child: Column(
+          children: [
+            _buildDrawerItem(Icons.person, "내 정보"),
+            _buildDrawerItem(Icons.person, "내 게시글 보기"),
+            _buildDrawerItem(Icons.settings, "설정"),
+          ],
         ),
       ),
     );
   }
+}
+
+Widget _buildDrawerItem(IconData iconData, String content) {
+  return Container(
+    decoration: const BoxDecoration(border: Border(bottom: BorderSide(color: Colors.black12))),
+    child: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          Icon(iconData),
+          const SizedBox(width: 5.0),
+          Text(content),
+        ],
+      ),
+    ),
+  );
 }
